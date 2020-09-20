b76e886553e36b02a3238a807dad2703b4be0d71
luizhenriqque18 <luizhenriqque15@gmail.com>
2020-09-19 23:47:39 -0400

    Updates

---
+++ precache-manifest.6b009478d51595a785fa078b6d0a14a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36044782e7690b23c47c955997159f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "750908f7b0c28008a573",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "420c2bab66e360c4049f",
+    "url": "/static/js/2.5f0e3010.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f0e3010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750908f7b0c28008a573",
+    "url": "/static/js/main.67f98240.chunk.js"
+  },
+  {
+    "revision": "dd3441b6c7752f4a5130",
+    "url": "/static/js/runtime-main.8042971a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
